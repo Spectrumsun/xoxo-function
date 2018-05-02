3143a7192323ffc0899c3d78ad417c98058c6aef
sunday taiwo <taiwo.sunday@outlook.comm>
2018-05-02 08:55:47 +0100

    xoxo function with test

---
+++ test.js
@@ -0,0 +1,55 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const chai  = require( 'chai');
+const xoxo = require('./xoxo');
+
+const { expect, should } = chai;
+
+
+describe('XOXO Function', () => {
+
+    it('should exist', () => {
+        expect(xoxo).to.not.be.undefined;
+    });
+
+    // it('should return a boolean ', function() {
+    //     const test = xoxo('rama', 'x', 'o')
+    //     expect(test).to.eql(true);
+    // });
+
+    it('should return a true when no x or o is present ', () => {
+        const test = xoxo('rama', 'x', 'o')
+        expect(test).to.eql(true);
+    });
+
+    it('should return a false if the numbers of o is greater than x', () => {
+        const test = xoxo('xxooo', 'x', 'o')
+        expect(test).to.eql(false);
+    });
+
+    it('should return a false if the numbers of x is greater than o', () => {
+        const test = xoxo('xxxxo', 'x', 'o')
+        expect(test).to.eql(false);
+    });
+
+    it('should return a true if the numbers of x the same has the number of o', () => {
+        const test = xoxo('xoxo', 'x', 'o')
+        expect(test).to.eql(true);
+    });
+
+    it('should return a true if the x is in capital letters but still the same amount of o ', () => {
+        const test = xoxo('xoXo', 'x', 'o')
+        expect(test).to.eql(true);
+    });
+
+    it('should return a true if the o is in capital letters but still the same amount of x ', () => {
+        const test = xoxo('OOOxxx', 'x', 'o')
+        expect(test).to.eql(true);
+    });
+
+    it('should return a true if the they are no x or x', () => {
+        const test = xoxo('OOOxxx', 'x', 'o')
+        expect(test).to.eql(true);
+    });
+
+});
---
+++ xoxo.js
@@ -0,0 +1,37 @@
+// *Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.*
+
+// _Examples input/output:_
+
+// ```XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false```
+
+module.exports =  xoxo = (letter, check1, check2) => {
+  if(letter !== 'string'){
+    return 'Only string allowed'
+  }else {
+    const arg = letter.toLowerCase()
+    const countx = [];
+    const counto = [];
+    for(let i in arg){
+      if(arg[i] === check1){
+        countx.push(arg[i])
+      }
+       if(arg[i] === check2){
+        counto.push(arg[i])
+      }
+      
+    }
+    return countx.length === counto.length ? true : false
+  }
+}
+
+console.log(xoxo('ooxx' ,'x', "o"))
+console.log(xoxo('xooxx' ,'x', "o"))
+console.log(xoxo('ooxXm' ,'x', "o"))
+console.log(xoxo('zpzpzpp' ,'x', "o"))
+console.log(xoxo('zzoo' ,'x', "o"))
+
+console.log(xoxo('baba' ,'x', "o"))
